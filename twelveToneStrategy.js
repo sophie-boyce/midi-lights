04b25d1c7619e2c4087660ab4f5b03f10f4ca5af
sophie-boyce <sophia.m.boyce@gmail.com>
2016-12-24 15:11:43 -0700

    finished first lighting strategy

--- twelveToneStrategy.js
+++ twelveToneStrategy.js
@@ -9,21 +9,37 @@ var _toneSet = [];
 
 lights.setMasterBrightness(1.0);
 lights.allOFF();
+turnAllTonesOff();
+
+function turnAllTonesOff() {
+	for(var i = 0; i < 12; i++) {
+		_toneSet[i] = 0;
+	}
+}
 
 function midiHandler(message) {
 	var tone = message.note % 12;
 	lights.allOFF();
+
 	if(message.messageType == 'on')
-		_toneSet[tone] = true;
+		_toneSet[tone]++;
 	else if(message.messageType == 'off')
-		_toneSet[tone] = false;
+		_toneSet[tone]--;
 
 	for(var i = 0; i < 12; i++) {
-		if(_toneSet[i])
-			lights.setPixelRGB(i, 255, 0, 0);
+		if(_toneSet[i] > 0)
+			turnOnLightsForTone(i);
 	}
 	
 	lights.update();
+}
+
+function turnOnLightsForTone(tone) {
+	var i = tone;
+	while(i < _totalLights) {
+		lights.setPixelRGB(i, 255, 0, 0);
+		i = i + 12;
+	}
 }
 
 function strategy(totalLights) {
