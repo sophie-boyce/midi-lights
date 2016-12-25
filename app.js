92377b0cfa8c6faf9fe4d3c1fe444f4c33c20e91
sophie-boyce <sophia.m.boyce@gmail.com>
2016-12-25 16:35:40 -0700

    added ability to supply color strategies

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var midiFilter = require('./midiFilter.js');
-var lightStrategy = require('./twelveToneStrategy.js');
+var light = require('./twelveToneStrategy.js');
+var color = require('./colorStrategy');
 
-var strategy = lightStrategy(160);
+var colorStrategy = color(0, 255, 0, 1.0);
+var lightStrategy = light(160, colorStrategy);
 
 midiFilter.openPort();
 
-midiFilter.on('midiNote', strategy.midiHandler); 
+midiFilter.on('midiNote', lightStrategy.midiHandler); 
 
 process.stdin.on('data', function (text) {
     midiFilter.closePort();
--- twelveToneStrategy.js
+++ twelveToneStrategy.js
@@ -5,6 +5,7 @@ var LightApi = require('lpd8806');
 var lights = new LightApi(160, '/dev/spidev0.0');
 
 var _totalLights;
+var _colorStrategy;
 var _toneSet = [];
 
 lights.setMasterBrightness(1.0);
@@ -21,6 +22,9 @@ function midiHandler(message) {
 	var tone = message.note % 12;
 	lights.allOFF();
 
+	var colors = _colorStrategy.getColors(message);
+	lights.setMasterBrightness(colors.brightness);
+
 	if(message.messageType == 'on')
 		_toneSet[tone]++;
 	else if(message.messageType == 'off')
@@ -28,22 +32,23 @@ function midiHandler(message) {
 
 	for(var i = 0; i < 12; i++) {
 		if(_toneSet[i] > 0)
-			turnOnLightsForTone(i);
+			turnOnLightsForTone(i, colors.red, colors.green, colors.blue);
 	}
 	
 	lights.update();
 }
 
-function turnOnLightsForTone(tone) {
+function turnOnLightsForTone(tone, red, green, blue) {
 	var i = tone;
 	while(i < _totalLights) {
-		lights.setPixelRGB(i, 255, 0, 0);
+		lights.setPixelRGB(i, red, green, blue);
 		i = i + 12;
 	}
 }
 
-function strategy(totalLights) {
+function strategy(totalLights, colorStrategy) {
 	_totalLights = totalLights;
+	_colorStrategy = colorStrategy;
 
 	return {
 		midiHandler: midiHandler
