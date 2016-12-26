5fbdddeb70906eb3f08b25f900e4a9d99e2b9da7
sophie-boyce <sophia.m.boyce@gmail.com>
2016-12-25 17:30:23 -0700

    added a christmas color strategy

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var midiFilter = require('./midiFilter.js');
 var light = require('./twelveToneStrategy.js');
-var color = require('./colorStrategy');
+//var color = require('./colorStrategy');
+var color = require('./christmasColorStrategy');
 
-var colorStrategy = color(0, 255, 0, 1.0);
+//var colorStrategy = color(0, 255, 0, 1.0);
+var colorStrategy = color();
 var lightStrategy = light(160, colorStrategy);
 
 midiFilter.openPort();
